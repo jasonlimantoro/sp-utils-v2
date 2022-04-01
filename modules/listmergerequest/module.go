744b47633e04f8a52b7a954d900384ab52c9aa48
Jason Limantoro <jason.limantoro@shopee.com>
2022-04-01 16:50:48 +0800

    [merge-request list] support multiple repositories using short names

--- modules/listmergerequest/module.go
+++ modules/listmergerequest/module.go
@@ -29,48 +29,52 @@ func NewModule(repositorydm repository.Manager) *module {
 }
 
 func (m module) Do(ctx context.Context, args *Args) error {
-	repositoryData, err := m.repositorydm.GetByName(ctx, args.Repository)
-	if err != nil {
-		return errlib.WrapFunc(err)
-	}
-
-	matchingMergeRequests, err := m.repositorydm.ListMergeRequests(
-		ctx,
-		repositoryData.ProjectID,
-		args.JiraTicketIDs,
-		args.State,
-	)
-	if err != nil {
-		return errlib.WrapFunc(err)
-	}
-
-	bySourceBranch := make(map[string][]*repository.MergeRequest)
-
-	for _, mr := range matchingMergeRequests {
-		if _, ok := bySourceBranch[mr.SourceBranch]; !ok {
-			bySourceBranch[mr.SourceBranch] = []*repository.MergeRequest{mr}
-		} else {
-			bySourceBranch[mr.SourceBranch] = append(bySourceBranch[mr.SourceBranch], mr)
+	for _, repoShortName := range args.Repository {
+		repositoryPath := repository.RepoToPathMapping[repoShortName]
+		repositoryData, err := m.repositorydm.GetByName(ctx, repositoryPath)
+		if err != nil {
+			return errlib.WrapFunc(err)
 		}
-	}
+		matchingMergeRequests, err := m.repositorydm.ListMergeRequests(
+			ctx,
+			repositoryData.ProjectID,
+			args.JiraTicketIDs,
+			args.State,
+		)
+		if err != nil {
+			return errlib.WrapFunc(err)
+		}
+		bySourceBranch := make(map[string][]*repository.MergeRequest)
 
-	for sourceBranch, mergeRequests := range bySourceBranch {
-		for _, mr := range mergeRequests {
-			fmt.Printf("%s|%s: %s (%s)\n", mr.TargetBranch, mr.Title, mr.WebURL, mr.State)
+		for _, mr := range matchingMergeRequests {
+			if _, ok := bySourceBranch[mr.SourceBranch]; !ok {
+				bySourceBranch[mr.SourceBranch] = []*repository.MergeRequest{mr}
+			} else {
+				bySourceBranch[mr.SourceBranch] = append(bySourceBranch[mr.SourceBranch], mr)
+			}
 		}
 
-		fmt.Println()
+		if len(bySourceBranch) > 0 {
+			fmt.Println("->", repositoryPath)
+		}
+		for sourceBranch, mergeRequests := range bySourceBranch {
+			for _, mr := range mergeRequests {
+				fmt.Printf("%s|%s: %s (%s)\n", mr.TargetBranch, mr.Title, mr.WebURL, mr.State)
+			}
 
-		fmt.Printf("Branch comparison links for %s: \n", sourceBranch)
+			fmt.Println()
 
-		for _, targetBranch := range TargetBranchToCompare {
-			fmt.Printf("%s|https://%s/%s/-/compare/%s...%s\n",
-				targetBranch,
-				gitlab.GitlabHost,
-				args.Repository,
-				targetBranch,
-				url.QueryEscape(sourceBranch),
-			)
+			fmt.Printf("Branch comparison links for %s: \n", sourceBranch)
+
+			for _, targetBranch := range TargetBranchToCompare {
+				fmt.Printf("%s|https://%s/%s/-/compare/%s...%s\n",
+					targetBranch,
+					gitlab.GitlabHost,
+					repositoryPath,
+					targetBranch,
+					url.QueryEscape(sourceBranch),
+				)
+			}
 		}
 	}
 
@@ -78,19 +82,15 @@ func (m module) Do(ctx context.Context, args *Args) error {
 }
 
 type Args struct {
-	Repository    string
+	Repository    []string
 	JiraTicketIDs []string
 	State         string
 }
 
 func (a *Args) FromMap(flags map[string]string) *Args {
-	repositoryVal, _ := flags["repository"]
-	jiraVal, _ := flags["jira"]
-	stateVal, _ := flags["state"]
-
-	a.Repository = repositoryVal
-	a.JiraTicketIDs = strings.Split(jiraVal, ",")
-	a.State = stateVal
+	a.Repository = strings.Split(flags["repository"], ",")
+	a.JiraTicketIDs = strings.Split(flags["jira"], ",")
+	a.State = flags["state"]
 
 	return a
 }
